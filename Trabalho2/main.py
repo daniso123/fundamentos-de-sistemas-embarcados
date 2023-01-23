cef3cdda966c8848ead0e15318e4a19b4f9ffdcd
06468709121 <daniela.soares@economia.gov.br>
2023-01-23 12:46:34 -0300

    refatorando main

--- Trabalho2/main.py
+++ Trabalho2/main.py
@@ -1,302 +1,146 @@
-import serial
 import time
-import datetime
 import struct
-import math
+from threading import Thread, Event
 
-from threading import Event, Thread 
+from utilitarios.teperaturaexterna import LeitorTemperaturaExterna
+import utilitarios.csv
+import conexão.uart
+import conexão.forno
+import utilitarios.pid
+import utilitarios.controle
 
-import bme280
-import smbus2
-from conexão.uart import UART
-from utilitarios.pid import PID
-from conexão.forno import Forno
-from utilitarios.csv import CSV
+pid = utilitarios.pid.PID()
+leitor_temperatura_externa = LeitorTemperaturaExterna.LeitorTemperaturaExterna()
+log = utilitarios.csv.LogManager()
+communicator = conexão.uart.UartCommunicator()
+temperature_controller = utilitarios.controle.TemperatureControlModule()
 
-class Esquenta:
-    port = '/dev/serial0'
-    baudrate = 9600
-    timeout = 0.5
-    matricula = [5, 2, 2, 2]
 
-    ligado = Event()
-    funcionando = Event()
-    aquecendo = Event()
-    resfriando = Event()
-    temporizador = Event()
-    enviando = Event()
-    
-    port = 1
-    address = 0x76
-    bus = smbus2.SMBus(port)
+def turn_on():
+    print("Oven set on")
+    turn_on_code = b'\x01\x23\xd3'
+    communicator.send_code(turn_on_code, b'\x01', 8)
+    data_received = communicator.message_receiver()
+    received_int = int.from_bytes(data_received, 'little')
+    print(received_int)
 
-    calibration_params = bme280.load_calibration_params(bus, address)
 
-   
+def turn_off():
+    print("Oven set off")
+    turn_off_code = b'\x01\x23\xd3'
+    communicator.send_code(turn_off_code, b'\x00', 8)
+    data_received = communicator.message_receiver()
+    received_int = int.from_bytes(data_received, 'little')
+    print(received_int)
 
-    bme=bme280.sample(bus, address, calibration_params)
-    
-    menu = -1
-    
-    temp_inter = 0
-    temp_ref = 0
 
-    tempo_seg = 0
-    tempo_ref = 0
-    def __init__(self):
-        self.uart = UART(self.port, self.baudrate, self.timeout)
-        self.pid = PID()
-        self.forno = Forno()
-        self.csv = CSV()
-        self.inicia_servicos()
+def start_work():
+    print("Oven started working")
+    turn_on_code = b'\x01\x23\xd5'
+    communicator.send_code(turn_on_code, b'\x01', 8)
+    data_received = communicator.message_receiver()
+    received_int = int.from_bytes(data_received, 'little')
+    print(received_int)
 
-    def liga(self):
-        self.enviando.set()
-        comando_estado = b'\x01\x23\xd3'
 
-        self.uart.envia(comando_estado, self.matricula, b'\x01', 8)
-        dados = self.uart.recebe()
+def stop_work():
+    print("Oven stop working")
+    turn_on_code = b'\x01\x23\xd5'
+    communicator.send_code(turn_on_code, b'\x00', 8)
+    data_received = communicator.message_receiver()
+    received_int = int.from_bytes(data_received, 'little')
+    print(received_int)
 
-        if dados is not None:
-            self.para()
-            self.seta_tempo(0)
-            self.ligado.set()
 
-        self.enviando.clear()
+def send_control_signal(signal_intensity):
 
-    def desliga(self):
-        self.enviando.set()
-        comando_estado = b'\x01\x23\xd3'
+    send_control_signal_code = b'\x01\x23\xd1'
+    valor = (round(signal_intensity)).to_bytes(4, 'little', signed=True)
+    communicator.send_code(send_control_signal_code, valor, 11)
+    data_received = communicator.message_receiver()
 
-        self.uart.envia(comando_estado, self.matricula, b'\x00', 8)
-        dados = self.uart.recebe()
 
-        if dados is not None:
-            self.para()
-            self.ligado.clear()
+def watch_for_buttons(oven):
 
-        self.enviando.clear()
+    request_buttons_code = b'\x01\x23\xc3'
+    communicator.send_code(request_buttons_code)
+    data_received = communicator.message_receiver()
 
-    def inicia(self):
-        self.enviando.set()
-        comando_estado = b'\x01\x23\xd5'
+    if data_received is not None:
 
-        self.uart.envia(comando_estado, self.matricula, b'\x01', 8)
-        dados = self.uart.recebe()
+        button = int.from_bytes(data_received, 'little')
 
-        if dados is not None:
-            self.funcionando.set()
+        if button == 161:
+            print("[1] pressed")
+            oven.on = True
+            turn_on()
+        elif button == 162:
+            print("[2] pressed")
+            oven.on = False
+            turn_off()
+            oven.working = False
+            stop_work()
+        elif button == 163:
+            print("[3] pressed")
+            oven.working = True
+            start_work()
+        elif button == 164:
+            print("[4] pressed")
+            oven.working = False
+            stop_work()
 
-        self.enviando.clear()
+    time.sleep(0.5)
 
-    def para(self):
-        self.enviando.set()
-        comando_estado = b'\x01\x23\xd5'
 
-        self.uart.envia(comando_estado, self.matricula, b'\x00', 8)
-        dados = self.uart.recebe()
+def read_and_update_temperature_target(oven):
+    request_temperature_target_code = b'\x01\x23\xc2'
+    communicator.send_code(request_temperature_target_code)
+    data_received = communicator.message_receiver()
+    temp = struct.unpack('f', data_received)[0]
+    oven.oven_temperature_target = temp
+    print("Target Temperature - " + str(temp))
 
-        if dados is not None:
-            self.funcionando.clear()
 
-        self.enviando.clear()
+def read_and_update_oven_temperature(oven):
+    request_oven_temperature_code = b'\x01\x23\xc1'
+    communicator.send_code(request_oven_temperature_code)
+    data_received = communicator.message_receiver()
+    temp = struct.unpack('f', data_received)[0]
+    oven.internal_temperature = temp
+    print("Oven temperature - " + str(temp))
 
-    def abre_menu(self):
-        pass
 
-    def envia_sinal_controle(self, pid):
-        self.enviando.set()
-        comando_aquec = b'\x01\x23\xd1'
-        valor = (round(pid)).to_bytes(4, 'little', signed=True)
+def system_update_routine():
 
-        self.uart.envia(comando_aquec, self.matricula, valor, 11)
-        dados = self.uart.recebe()
+    oven = conexão.forno.ReflowOven()
 
-        self.enviando.clear()
+    while True:
 
-    def seta_forno(self):
-        if self.ligado.is_set():
-            if self.funcionando.is_set() and self.tempo_seg > 0:
-                pid = self.pid.pid_controle(self.temp_ref, self.temp_inter)
-                print('pid f', pid)
+        read_and_update_temperature_target(oven)
+        watch_for_buttons(oven)
+        read_and_update_oven_temperature(oven)
+        watch_for_buttons(oven)
+        pid_result = pid.output(oven.oven_temperature_target, oven.internal_temperature)
 
-                self.envia_sinal_controle(pid)
+        if oven.on and oven.working:
 
-                if math.isclose(self.temp_inter, self.temp_ref, rel_tol=1e-2):
-                    self.aquecendo.clear()
-                    self.resfriando.clear()
-                    self.temporizador.set()
-                elif self.temp_inter < self.temp_ref and not self.temporizador.is_set():
-                    self.aquecendo.set()
-                    self.resfriando.clear()
-                elif self.temp_inter > self.temp_ref and not self.temporizador.is_set():
-                    self.aquecendo.clear()
-                    self.resfriando.set()
 
-                if pid > 0:
-                    self.forno.aquecer(pid)
-                    self.forno.resfriar(0)
-                else:
-                    pid *= -1
-                    self.forno.aquecer(0)
-                    if pid < 40.0:
-                        self.forno.resfriar(40.0)
-                    else:
-                        self.forno.resfriar(pid)
+            print(pid_result)
+
+            send_control_signal(pid_result)
+
+            if pid_result > 0:
+                temperature_controller.heat_the_oven(pid_result)
+                temperature_controller.cool_the_oven(0)
             else:
-                pid = self.pid.pid_controle(27.0, self.temp_inter)
-                print('pid r', pid)
+                pid_result = pid_result * -1
+                if pid_result < 40:
+                    pid_result = 40
+                temperature_controller.cool_the_oven(pid_result)
+                temperature_controller.heat_the_oven(0)
 
-                if pid < 0:
-                    self.envia_sinal_controle(pid)
-                    pid *= -1
-                    self.forno.resfriar(pid)
-                    self.resfriando.set()
-                else:
-                    self.resfriando.clear()
-                self.forno.aquecer(0)
-                self.aquecendo.clear()
-                self.temporizador.clear()
-        else:
-            self.forno.aquecer(0)
-            self.forno.resfriar(0.0)
-            self.funcionando.clear()
-            self.aquecendo.clear()
-            self.resfriando.clear()
+        log.create_log_entry(leitor_temperatura_externa.get_external_temperature(), oven.internal_temperature, oven.oven_temperature_target, pid_result)
 
-    def conta_tempo(self):
-        while self.tempo_seg > 0:
-            time.sleep(1)
-            self.tempo_seg -= 1
-    
-    def seta_tempo(self, tempo):
-        self.enviando.set()
-        comando_estado = b'\x01\x23\xd6'
-        valor = tempo.to_bytes(4, 'little')
 
-        self.uart.envia(comando_estado, self.matricula, valor, 11)
-        dados = self.uart.recebe()
-
-        self.tempo_ref = tempo
-        self.tempo_seg = tempo * 60
-
-        self.enviando.clear()
-    
-    def trata_botao(self, bytes):
-        botao = int.from_bytes(bytes, 'little')
-        print('botao', botao)
-        if botao == 1:
-            self.liga()
-        elif botao == 2:
-           self.desliga()
-        elif botao == 3:
-            self.inicia()
-        elif botao == 4:
-            self.para()
-        elif botao == 5:
-            tempo = self.tempo_ref + 1
-            self.seta_tempo(tempo + 1)
-        elif botao == 6:
-            tempo = self.tempo_ref - 1
-            if tempo < 0:
-                tempo = 0
-            self.seta_tempo(tempo - 1)
-        elif botao == 7:
-            self.abre_menu()
-
-    def trata_temp_int(self, bytes):
-        temp = struct.unpack('f', bytes)[0]
-        print('temperatura int', self.temp_inter)
-
-        if temp > 0 and temp < 100:
-            self.temp_inter = temp
-        
-        self.seta_forno()
-        if self.temporizador.is_set():
-            self.conta_tempo()
-
-    def trata_temp_ref(self, bytes):
-        temp = struct.unpack('f', bytes)[0]
-        print('temperatura ref', temp)
-
-        if temp > 0 and temp < 100:
-            self.temp_ref = temp
-    
-    def solicita_botao(self):
-        comando_botao = b'\x01\x23\xc3'
-        
-        self.uart.envia(comando_botao, self.matricula, b'', 7)
-        dados = self.uart.recebe()
-
-        if dados is not None:
-            self.trata_botao(dados)
-
-    def solicita_temp_int(self):
-        comando_temp = b'\x01\x23\xc1'
-
-        self.uart.envia(comando_temp, self.matricula, b'', 7)
-        dados = self.uart.recebe()
-
-        if dados is not None:
-            self.trata_temp_int(dados)
-
-    def solicita_temp_ref(self):
-        comando_temp = b'\x01\x23\xc2'
-
-        self.uart.envia(comando_temp, self.matricula, b'', 7)
-        dados = self.uart.recebe()
-
-        if dados is not None:
-            self.trata_temp_ref(dados)
-
-    def atualiza_bme(self):
-        while True:
-            if self.ligado.is_set():
-                self.bme.clear()
-                if self.aquecendo.is_set():
-                    self.bme.text(f'TI:{round(self.temp_inter, 2)} TR:{round(self.temp_ref, 2)}', 1)
-                    self.bme.text(f'Pre-aquecendo', 2)
-                elif self.resfriando.is_set():
-                    self.bme.text(f'TI:{round(self.temp_inter, 2)} TR:{round(self.temp_ref, 2)}', 1)
-                    self.bme.text(f'Resfriando', 2)
-                else:
-                    self.bme.text(f'TI:{round(self.temp_inter, 2)} TR:{round(self.temp_ref, 2)}', 1)
-                    self.bme.text(f'Tempo: {str(datetime.timedelta(seconds=self.tempo_seg))}', 2)
-            else:
-                self.bme.clear()
-            time.sleep(1)
-
-    def salva_log(self):
-        header = ['data', 'ti', 'tr', 'resistor/ventoinha']
-        self.csv.escrever(header)
-        
-        while True:
-            data = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-            linha = [data, self.temp_inter, self.temp_ref, self.pid.sinal_de_controle]
-            self.csv.escrever(linha)
-            time.sleep(1)
-
-    def rotina(self):
-        while True:
-            self.solicita_botao()
-            time.sleep(0.5)
-            self.solicita_botao()
-            time.sleep(0.5)
-            self.solicita_temp_int()
-            self.solicita_temp_ref()
-    
-    def inicia_servicos(self):
-        self.liga()
-
-        thread_rotina = Thread(target=self.rotina, args=())
-        thread_rotina.start()
-
-        thread_bme = Thread(target=self.atualiza_bme, args=())
-        thread_bme.start()
-
-        thread_csv = Thread(target=self.salva_log, args=())
-        thread_csv.start()
-
-        print('Forno iniciada')
-
-Esquenta()
+system_routine_thread = Thread(target=system_update_routine, args=())
+system_routine_thread.start()
