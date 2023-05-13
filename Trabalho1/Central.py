24e6b3d5274b5e7079337bcbb93baa0d0855c9bc
daniela.oliveira <daniela.oliveira@dmz.org.br>
2023-05-12 22:19:48 -0300

    tentando conectar tc/ip

--- Trabalho1/Central.py
+++ Trabalho1/Central.py
@@ -1,5 +1,5 @@
+from threading import Thread
 import socket
-
 
 
 ip_servidor = '164.41.98.15'
@@ -16,3 +16,16 @@ print("Aguardando servidor distribuido . . . ")
 (clientConnected, clientAddress) = serverSocket.accept()
 
 print("Distribuido Conectado")
+
+def receive_messages(fila_respostas):
+    while True:
+        dataFromClient = clientConnected.recv(1024)
+        #print("Data received = " + dataFromClient.decode())
+        fila_respostas.append(json.loads(dataFromClient.decode()))
+        sleep(0.15)
+
+t = Thread(target=send_messages, args=(fila_instrucoes, ))
+t.start()
+
+t2 = Thread(target=receive_messages, args=(fila_respostas, ))
+t2.start()
