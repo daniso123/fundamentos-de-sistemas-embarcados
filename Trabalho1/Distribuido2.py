695b2ce72e16144cf5bc8097aa7dc8f2aeebe000
06468709121 <dannysoaresdeoliveira@gmail.com>
2023-05-15 10:32:47 -0300

    arrumando o codigo binario

--- Trabalho1/Distribuido2.py
+++ Trabalho1/Distribuido2.py
@@ -47,9 +47,9 @@ class Cliente:
         self.cliente_socket = None
 
     def leitura_sensor_vaga(self,endereco):
-        GPIO.output(ENDERECO_01, (endereco & 0b001) == 0b001)
-        GPIO.output(ENDERECO_02, (endereco & 0b010) == 0b010)
-        GPIO.output(ENDERECO_03, (endereco & 0b100) == 0b100)
+        GPIO.output(int(ENDERECO_01), (int(endereco) & 0b001) == 0b001)
+        GPIO.output(int (ENDERECO_02), (int(endereco) & 0b010) == 0b010)
+        GPIO.output(int(ENDERECO_03), (int(endereco)  & 0b100) == 0b100)
         time.sleep(0.2) 
         return GPIO.input(SENSOR_DE_VAGA)
                         
