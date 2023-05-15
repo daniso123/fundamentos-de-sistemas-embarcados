b2205b354e9ccb6730a56d3814171de0ad848266
06468709121 <dannysoaresdeoliveira@gmail.com>
2023-05-15 20:02:03 -0300

    revertendo a refatoração

--- Trabalho1/Distribuido2.py
+++ Trabalho1/Distribuido2.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 import time
 import json
 
+
 # Configuração dos pinos GPIO
 GPIO.setmode(GPIO.BCM)
 
@@ -20,6 +21,9 @@ SENSOR_FECHAMENTO_CANCELA_SAIDA = 12
 MOTOR_CANCELA_SAIDA = 17
 
 # Configuração dos pinos GPIO
+GPIO.setmode(GPIO.BCM)
+
+# Configuração dos pinos como saída ou entrada
 GPIO.setup(ENDERECO_01, GPIO.OUT)
 GPIO.setup(ENDERECO_02, GPIO.OUT)
 GPIO.setup(ENDERECO_03, GPIO.OUT)
@@ -32,27 +36,27 @@ GPIO.setup(SENSOR_ABERTURA_CANCELA_SAIDA, GPIO.IN)
 GPIO.setup(SENSOR_FECHAMENTO_CANCELA_SAIDA, GPIO.IN)
 GPIO.setup(MOTOR_CANCELA_SAIDA, GPIO.OUT)
 
-carros_andar = 0
-sinal1 = 0
-id_carro = 0
+carros_andar = 0; sinal1 = 0; id_carro = 0
 vagas = {'a1': 0, 'a2': 0, 'a3': 0, 'a4': 0, 'a5': 0, 'a6': 0, 'a7': 0, 'a8': 0}
 
 class Cliente:
-    def __init__(self, servidor, porta, name):
+    def __init__(self, servidor, porta,name):
         self.servidor = servidor
         self.porta = porta
         self.name = name
         self.cliente_socket = None
 
-    def leitura_sensor_vaga(self, endereco):
-        GPIO.output(ENDERECO_01, (int(endereco) & 0b001) == 0b001)
-        GPIO.output(ENDERECO_02, (int(endereco) & 0b010) == 0b010)
-        GPIO.output(ENDERECO_03, (int(endereco) & 0b100) == 0b100)
-        time.sleep(0.2)
+    def leitura_sensor_vaga(self,endereco):
+        GPIO.output(int(ENDERECO_01), (int(endereco) & 0b001) == 0b001)
+        GPIO.output(int (ENDERECO_02), (int(endereco) & 0b010) == 0b010)
+        GPIO.output(int(ENDERECO_03), (int(endereco)  & 0b100) == 0b100)
+        time.sleep(0.2) 
         return GPIO.input(SENSOR_DE_VAGA)
+                        
 
     def iniciar_cliente(self):
-        global sinal1, carros_andar, id_carro, vagas
+        global sinal1, carros_andar
+        global id_carro, sinal1, vagas, id_carro
 
         self.cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.cliente_socket.connect((self.servidor, self.porta))
@@ -60,33 +64,41 @@ class Cliente:
 
         thread_recebimento = threading.Thread(target=self.receber_mensagens)
         thread_recebimento.start()
+        
 
         while True:
-            # ENTRADA DE CARROS
+            
+            
+            #ENTRADA DE CARROS
             if GPIO.input(SENSOR_ABERTURA_CANCELA_ENTRADA) == GPIO.HIGH:
                 GPIO.output(MOTOR_CANCELA_ENTRADA, GPIO.HIGH)
                 envio = "O carro passou"
-                vaga_ocupada = True
-                self.enviar_mensagem(codigo, carros_andar, sinal1, vagas, id_carro, vg)
-                                # Cria uma nova thread para enviar os números de carros no andar 1
-                codigo = "entrada"
-                self.enviar_mensagem(codigo, carros_andar, sinal1, vagas, id_carro, vaga_ocupada)
+                vaga_ocupada= True
+                self.enviar_mensagem(envio, carros_andar,sinal1, vagas, id_carro, vaga_ocupada)
 
+                
                 print("entrando um carro")
 
-                carros_andar += 1
-
+                carros_andar = carros_andar + 1
+                
+                # Cria uma nova thread para enviar os números de carros no andar 1
+                codigo = "entrada"
+                self.enviar_mensagem(codigo, carros_andar, sinal1, vagas, id_carro, 0)
+                
                 # Verifica se o sensor de fechamento da cancela de entrada foi acionado
                 if GPIO.wait_for_edge(SENSOR_FECHAMENTO_CANCELA_ENTRADA, GPIO.RISING):
                     # Desativa o motor da cancela de entrada para fechar a cancela
                     GPIO.output(MOTOR_CANCELA_ENTRADA, GPIO.LOW)
                 time.sleep(4)
-
+                
+                #for endereco in range(8):
+                 #   if self.leitura_sensor_vaga(endereco):
+                  #      vagas.append(endereco+1)
                 for i in range(8):
                     endereco = bin(i)[2:].zfill(3)  # Converte i para binário e adiciona zeros à esquerda para formar uma string de 3 dígitos
-                    GPIO.output(ENDERECO_01, (int(endereco) & 0b001) == 0b001)
-                    GPIO.output(ENDERECO_02, (int(endereco) & 0b010) == 0b010)
-                    GPIO.output(ENDERECO_03, (int(endereco) & 0b100) == 0b100)
+                    GPIO.output(int(ENDERECO_01), (int(endereco) & 0b001) == 0b001)
+                    GPIO.output(int (ENDERECO_02), (int(endereco) & 0b010) == 0b010)
+                    GPIO.output(int(ENDERECO_03), (int(endereco)  & 0b100) == 0b100)
                     time.sleep(0.2)
                     ocupada = GPIO.input(SENSOR_DE_VAGA)
                     vg = 'a' + str(i+1)
@@ -101,6 +113,7 @@ class Cliente:
                             id_carro = 0
                             break
                         else:
+
                             if vagas[vg] == 1:
                                 print(f"Saindo um carro da vaga {vg}")
                                 vagas[vg] = 0
@@ -108,30 +121,37 @@ class Cliente:
                                 self.enviar_mensagem(codigo, carros_andar, sinal1, vagas, id_carro, vg)
                                 break
 
+
                 if GPIO.wait_for_edge(SENSOR_FECHAMENTO_CANCELA_ENTRADA, GPIO.RISING):
                     GPIO.output(MOTOR_CANCELA_ENTRADA, GPIO.LOW)
 
-            # SAÍDA DE CARROS
+            #SAÍDA DE CARROS
             if GPIO.input(SENSOR_ABERTURA_CANCELA_SAIDA) == GPIO.HIGH:
                 GPIO.output(MOTOR_CANCELA_SAIDA, GPIO.HIGH)
                 if GPIO.wait_for_edge(SENSOR_FECHAMENTO_CANCELA_SAIDA, GPIO.RISING):
                     GPIO.output(MOTOR_CANCELA_SAIDA, GPIO.LOW)
+            
 
+
+   
+    
     def enviar_mensagem(self, codigo, carros_andar, sinal1, vagas, id_carro, vaga_ocupada):
         dados = [{
-            "cod": codigo,
-            "carros_andar1": carros_andar,
-            "sinal1": sinal1,
-            "vagas": vagas,
-            "id": id_carro,
-            "vaga_ocupada": vaga_ocupada
-        }]
-
+                    "cod": codigo,
+                    "carros_andar1": carros_andar,
+                    "sinal1": sinal1,
+                    "vagas": vagas,
+                    "id": id_carro,
+                    "vaga_ocupada": vaga_ocupada
+                 
+                 }]
+        
         messages = {
             "from": self.name,
             "message": dados
         }
         self.cliente_socket.send(json.dumps(messages).encode())
+        
 
     def receber_mensagens(self):
         while True:
@@ -146,7 +166,6 @@ class Cliente:
 
 cliente = Cliente("localhost", 10231, 'Client 1')
 cliente.iniciar_cliente()
-
 
 
 
