4e25cde8db69a58eef8a6b24ee82bd56aa8600d4
06468709121 <dannysoaresdeoliveira@gmail.com>
2023-05-15 22:22:47 -0300

    revertendo a refatoração

--- Trabalho1/Distribuido2.py
+++ Trabalho1/Distribuido2.py
@@ -164,8 +164,17 @@ class Cliente:
 
 # Exemplo de uso
 
-cliente = Cliente("localhost", 10231, 'Client 1')
-cliente.iniciar_cliente()
+#cliente = Cliente("localhost", 10231, 'Client 1')
+#cliente.iniciar_cliente()
+
+
+def main():
+    cliente = Cliente("localhost", 10231, 'Client 1')
+    cliente_thread = threading.Thread(target=cliente.iniciar_cliente)
+    cliente_thread.start()
+
+if __name__ == '__main__':
+    main()
 
 ####################################Conexão com o servidor central############################################
 
